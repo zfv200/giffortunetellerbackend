1634fd15c31e90da45470c17e00f4b2e26e58ea2
github username <github email address>
2018-04-28 19:42:31 -0400

    fix in fortunes controller

--- app/controllers/api/v1/fortunes_controller.rb
+++ app/controllers/api/v1/fortunes_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::FortunesController < ApplicationController
     render json: Fortune.includes(:user), include: ['user']
   end
 
+  def create
+    @fortune = Fortune.create(fortune_params)
+    render json: @fortune
+  end
+
+  def fortune_data
+    @fortune = Fortune.find(params[:id])
+    render json: @fortune
+  end
+
+  private
+
+  def fortune_params
+    params.require(:fortune).permit(:gif, :message, :date, :user_id)
+  end
+
 end
