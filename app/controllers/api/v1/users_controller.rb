3b2522933901551cc3fa9b9351a7d40f906c2b45
github username <github email address>
2018-04-28 18:05:03 -0400

    finished controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::UsersController < ApplicationController
     render json: User.includes(:fortunes), include: ['fortunes']
   end
 
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def user_data
+		@user = User.find(params[:id])
+		render json: @user
+	end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
